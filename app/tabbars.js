150de9115453b0850f9eeadfee7489ce8b77f4ff
Nhan Nguyen <nhan.nguyen2@gmail.com>
2017-06-18 02:34:16 +0700

    Add searchbox, movie UI item

--- app/tabbars.js
+++ app/tabbars.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Button, ScrollView } from 'react-native';
+import React, { Component } from 'react';
+import { Button, ScrollView, Text, View, TextInput, ListView, Image, StyleSheet, Dimensions} from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import SampleText from './SampleText';
+import Search from 'react-native-search-box';
 
 const MyNavScreen = ({ navigation, banner }) => (
   <ScrollView>
     <SampleText>{banner}</SampleText>
     <Button
-      onPress={() => navigation.navigate('Profile', { name: 'Jordan' })}
+      onPress={() => navigation.navigate('Detail', { name: 'Jordan' })}
       title="Open profile screen"
     />
     <Button
@@ -24,13 +25,101 @@ const MyNavScreen = ({ navigation, banner }) => (
   </ScrollView>
 );
 
-const MyHomeScreen = ({ navigation }) => (
-  <MyNavScreen banner="Home Screen" navigation={navigation} />
-);
+let { height, width } = Dimensions.get('window');
+let imageWidth = 100;
+let imageHeight = 100;
+let colMargin = 5;
+let rightColWidth = width - 5 - imageWidth;
 
-const MyProfileScreen = ({ navigation }) => (
+const movieStyles = StyleSheet.create({
+  container: {
+    flex:1,
+    flexDirection: 'row',
+    marginTop: colMargin,
+    marginBottom: colMargin
+  },
+  image: {
+    width: imageWidth,
+    height: imageHeight
+  },
+  leftCol: {
+    flexDirection:'column',
+    marginRight: colMargin
+  },
+  rightCol: {
+    flexDirection:'column',
+    width: rightColWidth,
+    width: rightColWidth
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 10
+  },
+  content: {
+
+  }
+
+})
+
+
+
+class Movie extends Component {
+
+  render() {
+    return (
+      <View style={movieStyles.container}>
+        <View style={movieStyles.leftCol}>
+          <Image
+            style={movieStyles.image}
+            source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}>
+          </Image>
+        </View>
+
+        <View style={movieStyles.rightCol}>
+          <Text style={movieStyles.title}>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry
+          </Text>
+
+          <Text style={movieStyles.content}>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the
+          </Text>
+
+        </View>
+      </View>
+    )
+  }
+}
+
+class PlayingScreen extends Component {
+  constructor(props) {
+    super(props)
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => ( <Movie/> )}
+      />
+    )
+  }
+}
+
+// const PlayingScreen = ({ navigation }) => (
+//   <Text>aa</Text>
+// );
+
+const DetailMovieScreen = ({ navigation }) => (
   <MyNavScreen
-    banner={`${navigation.state.params.name}s Profile`}
+    banner= "DetailMovieScreen banner"
     navigation={navigation}
   />
 );
@@ -43,19 +132,29 @@ const MySettingsScreen = ({ navigation }) => (
   <MyNavScreen banner="Settings Screen" navigation={navigation} />
 );
 
-const MainTab = StackNavigator({
-  Home: {
-    screen: MyHomeScreen,
+const PlayingTab = StackNavigator({
+  Playing: {
+    screen: PlayingScreen,
     path: '/',
     navigationOptions: {
-      title: 'Welcome',
+      header: (props) => { return (
+        <View style={{marginTop: 20}}>
+          <Search
+            ref="search_box"
+            /**
+            * There many props that can customizable
+            * Please scroll down to Props section
+            */
+          />
+        </View>
+      ) }
     },
   },
-  Profile: {
-    screen: MyProfileScreen,
-    path: '/people/:name',
+  Detail: {
+    screen: DetailMovieScreen,
+    path: '/movies/:id',
     navigationOptions: ({ navigation }) => ({
-      title: `${navigation.state.params.name}'s Profile!`,
+      title: "Detail screen",
     }),
   },
 });
@@ -78,32 +177,24 @@ const SettingsTab = StackNavigator({
 
 const StacksInTabs = TabNavigator(
   {
-    MainTab: {
-      screen: MainTab,
+    PlayingTab: {
+      screen: PlayingTab,
       path: '/',
       navigationOptions: {
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-home' : 'ios-home-outline'}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        ),
+        tabBarLabel: 'Now Playing',
+        tabBarIcon: ({ tintColor, focused }) => {
+
+        }
       },
     },
     SettingsTab: {
       screen: SettingsTab,
       path: '/settings',
       navigationOptions: {
-        tabBarLabel: 'Settings',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-settings' : 'ios-settings-outline'}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        ),
+        tabBarLabel: 'Top Rated',
+        tabBarIcon: ({ tintColor, focused }) => {
+
+        }
       },
     },
   },
